ba5d418b7b7485c8b200d6f166d7c16e458a7d8c
radimkocko <159121941+radimkocko@users.noreply.github.com>
2024-09-16 09:39:23 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+jmeno = "Radim" #Proměnná se stringem (mým jménem).
+prijmeni = " Kočko" #Proměnná se stringem (mým příjmením).
+print(jmeno + prijmeni) #Vypíše proměnné do konzole.
+vek = int(input("Kolik je vám let?: "))
+print("Je vám: ", + vek, "let.")
+def vek():
+    while True:
+        vek
+        if vek.isdigit():
+            print("Okay")
+            break
+        else:
+            print("Zadej svůj věk číselnou hodnotou!")
